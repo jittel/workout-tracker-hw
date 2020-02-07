1ef69862888fb66c72c585070853e6ecc386b193
unknown <nathan.c.ginn@gmail.com>
2020-02-06 20:39:14 -0800

    start

---
+++ models/Workout.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: { type: Date, default: Date.now },
+    numExercises: { type: Number },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter an exercise duration in minutes"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+
+// adds a dynamically-created property to schema
+WorkoutSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const path = require('path');
+
+const db = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.normalize(__dirname + "/public/exercise.html"));
+});
+
+app.get('/stats', (req, res) => {
+    res.sendFile(path.normalize(__dirname + "/public/stats.html"));
+})
+
+app.get('/api/workouts', (req, res) => {
+    db.Workout.find({}).then(data => {
+        console.log(data);
+        res.json(data);
+    });
+});
+
+app.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({}).then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.status(404).json(err.message);
+    });
+});
+
+app.put('/api/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id,
+        { $push: { exercises: req.body } }, (err, data) => {
+            if (err) console.log(err);
+            else res.json(data);
+        })
+});
+
+app.post('/api/workouts', (req, res) => [
+    db.Workout.create(req.body, (err, data) => {
+        if (err) console.log(err);
+        else res.json(data);
+    })
+]);
+
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    else console.log('Listening on http://localhost:' + PORT);
+});
